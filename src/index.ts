356afdad15001061a23d35385c5c9700c6541409
molang404 <art104233@naver.com>
2026-03-26 12:15:49 +0900

    환경 변수 적용

--- src/index.ts
+++ src/index.ts
@@ -2,7 +2,7 @@ import express from 'express';
 import dotenv from "dotenv";
 
 // 1. 환경 변수 초기화
-dotenv.config();
+dotenv.config();       // .env 파일을 불러오려면 무조건 최상단에 있어야 함
 
 // 2. Express 앱 생성
 const app = express();
@@ -41,7 +41,7 @@ app.get("/", (req, res) => {
 
 // 3. app.listen : 서버를 실행하는 메소드
 //      매개변수 2개    (포트번호, 함수)
-app.listen(3000, () => {
+app.listen(process.env.PORT, () => {
     // 얘가 실행되면 처음할 일
-    console.log('서버가 실행되었습니다. http://localhost:3000');
+    console.log(`서버가 실행되었습니다. http://localhost:${process.env.PORT}`);
 });
